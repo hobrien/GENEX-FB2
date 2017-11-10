59c6f594f0fbd5e2cb14ed2ab9679b4d61c54855
Heath O'Brien <heath.obrien@gmail.com>
2017-11-10 13:30:56 +0000

    separate sequencing sample info from genotyping sample info

--- Bash/Peer.sh
+++ Bash/Peer.sh
@@ -12,6 +12,6 @@ Rscript /c8000xd3/rnaseq-heath/GENEX-FB2/R/PEER.R  -p diploid -n 25 \
   -o /c8000xd3/rnaseq-heath/GENEX-FB2/factors.txt \
   -r /c8000xd3/rnaseq-heath/GENEX-FB2/residuals.txt \
   -a /c8000xd3/rnaseq-heath/GENEX-FB2/alpha.txt \
-  -b /c8000xd3/rnaseq-heath/GENEX-FB2/Data/SampleInfo.1.txt \
+  -b /c8000xd3/rnaseq-heath/GENEX-FB2/Data/SampleInfo.txt \
   -e 17046,16385,17048,16024,16115
 
