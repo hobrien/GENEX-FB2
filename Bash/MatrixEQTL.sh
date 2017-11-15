70fd082a3504959e612895903094ee3b6e6c5833
hobrien <heath.obrien@gmail.com>
2017-11-15 15:25:27 +0000

    added missing line break

--- Bash/MatrixEQTL.sh
+++ Bash/MatrixEQTL.sh
@@ -7,7 +7,7 @@
 #
 
 Rscript R/MatrixEQTL.R  --genotypes Genotypes/Plink/recoded.traw \
-  --counts Data/MalevsFemale.complete.txt --snps Genotypes/Plink/genotypes.map 
+  --counts Data/MalevsFemale.complete.txt --snps Genotypes/Plink/genotypes.map \ 
   --genes Data/geneloc.txt --cofactors Data/SampleInfo.txt \
   --cis MatrixEQTL/cis_eqtl.txt --trans MatrixEQTL/trans_eqtl.txt \
   --image MatrixEQTL/results.RData
