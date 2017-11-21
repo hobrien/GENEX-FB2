3ffd07a1617809d52190f3b85d08d6b266b1e4c4
hobrien <heath.obrien@gmail.com>
2017-11-21 08:05:31 +0000

    allow additional arguments to Snakemake

--- snakemake.sh
+++ snakemake.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-snakemake --use-conda --cluster-config cluster_config.yaml --cluster "qsub -pe smp {cluster.num_cores} -l h_vmem={cluster.maxvmem}" -j 20 2> snakemake.log
+snakemake --use-conda --cluster-config cluster_config.yaml --cluster "qsub -pe smp {cluster.num_cores} -l h_vmem={cluster.maxvmem}" -j 20 $@ 2> snakemake.log
 mail -s "snakemake finished" heath.obrien@gmail.com < snakemake.log
